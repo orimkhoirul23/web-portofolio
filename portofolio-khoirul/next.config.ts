72fb28113cf3b8bc2b9a4f29b42b5725ee4bb3da
orimkhoirul <mkhoirulutfansyah@gmail.com>
2026-02-11 14:41:16 +0700

    Fix: Add basePath configuration for GitHub Pages

--- portofolio-khoirul/next.config.ts
+++ portofolio-khoirul/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "web-portofolio";
+
 const nextConfig: NextConfig = {
-  output: "export", // Wajib untuk GitHub Pages
+  output: "export",
+  images: { unoptimized: true },
+
+  // Mengarahkan Next.js agar selalu mencari file di dalam sub-folder repo
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `https://orimkhoirul23.github.io/${repoName}/` : "",
+
+  trailingSlash: true,
 };
 
 export default nextConfig;

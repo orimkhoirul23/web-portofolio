4d040b05e7371e456d7477b9426faad84dd9fa4b
orimkhoirul <mkhoirulutfansyah@gmail.com>
2026-02-11 14:41:39 +0700

    Updates

--- _next/static/6y8UN7xzrM4sQQd4lxiuS/_buildManifest.js
+++ _next/static/6y8UN7xzrM4sQQd4lxiuS/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/web-portofolio//_next/:path+",
+        "destination": "/web-portofolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
